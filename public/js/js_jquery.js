f422c9965e4b3dcbd5a517d493efcf316de183c3
John Bergandino <jbergandino@gmail.com>
2015-10-09 23:48:34 -0400

    started image carousel

--- public/js/js_jquery.js
+++ public/js/js_jquery.js
@@ -1,10 +1,42 @@
 $(document).ready(function(){
 
+	// var options = {
+ //        $ArrowNavigatorOptions: {
+ //            $Class: $JssorArrowNavigator$,
+ //            $ChanceToShow: 2
+ //        }
+ //    };
+
+ //    var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+
+
+
+
+        var options = {
+            $BulletNavigatorOptions: {
+                $Class: $JssorBulletNavigator$,
+                $ChanceToShow: 2
+            }
+        };
+        var jssor_slider1 = new $JssorSlider$('slider1_container', options);
 
 
 
 
 
+
+
+
+	// $(".home-images").on("click", function(){
+	// 		$(this).addClass("slide2").removeClass("slide1")
+	// });
+
+
+	// $(".lorem").hover(function(){
+	//     $(this).css("background-color", "yellow");
+	//     }, function(){
+	//     $(this).css("background-color", "pink");
+	// });
 
 
 
