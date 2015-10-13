86c53f705ea30b60b8f387fbdca7b19e9a916c24
John Bergandino <jbergandino@gmail.com>
2015-10-13 11:53:23 -0400

    added contact form page

--- Main.rb
+++ Main.rb
@@ -1,4 +1,8 @@
 require 'sinatra'
+require 'mandrill'
+
+api_key = ENV['MANDRILL_APIKEY']
+m = Mandrill::API.new "#{api_key}"
 
 get '/' do
 	@title = "Gardino's Gourmet Soup and Sandwiches"
@@ -18,14 +22,37 @@ get '/location' do
 	
 end
 
-get '/staff' do
-	@title = "Staff"
-	erb :staff
+get '/form' do
+	@title = "Contact Us"
+	erb :form
 	
 end
+
+
 
 get '/about' do
 	@title = "About"
 	erb :about
 	
+end
+
+post '/mail_page' do
+	@title = "Contact Us"
+	
+	
+	message = {
+ # :subject=> params[:username],
+ # :from_name=> "Your name",
+ :from_name=> params[:username],
+ :text=>params[:message_form],
+ :to=>[{:email=> "jbergandino@gmail.com", :name=> "JohnNameTest"}],
+ # :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",
+ # :from_email=>"jc@jasoncurran.com"
+ :from_email=>params[:email]
+
+}
+sending = m.messages.send message
+puts sending
+erb :mail_page
+
 end
